298c6fa2881b1ca87f5b39d90d955c32eb92c626
Anggara <51870433+AnggaraMAG@users.noreply.github.com>
2021-02-09 16:01:19 +0700

    error sweetalert conditional

--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Gap, Input, TextArea, Upload } from '../../components'
 import { BLUE, RED } from '../../utils/colors/constansColor'
 import { useHistory } from 'react-router-dom'
+import SweetAlert from 'react-bootstrap-sweetalert';
 import './createblog.scss'
 function CreateBlog() {
+  const [sweetWarning, setsweetWarning] = useState(false);
+  const [sweetSucces, setsweetSucces] = useState(false);
   const history = useHistory()
+
+
+
+  const _onConfirm = (param) => {
+    if (param === 'confirm') {
+      setsweetWarning(false);
+      setsweetSucces(true);
+    } else {
+      setsweetWarning(false);
+    }
+  }
+
   return (
     <div className="blog-wrapper">
+      <SweetAlert
+        show={sweetSucces}
+        success title="Good job!"
+        onConfirm={() => setsweetSucces(false)}
+        onCancel={() => setsweetSucces(false)}
+      >
+        You clicked the button!
+      </SweetAlert>
+      <SweetAlert
+        show={sweetWarning}
+        warning
+        showCancel
+        confirmBtnText="Yes Delete!"
+        confirmBtnBsStyle="danger"
+        cancelBtnBsStyle="default"
+        title="Are You Sure?"
+        onConfirm={_onConfirm('confirm')}
+        onCancel={_onConfirm('cancel')}
+      >
+        You will not be able to recover this imaginary file!
+      </SweetAlert>
       <div className="title-create-wrapper">
         <p className="tittle-create">Created Blog</p>
       </div>
@@ -21,9 +57,9 @@ function CreateBlog() {
       <div className="buttons-wrapper">
         <Button label="Cancel" background={RED} height={35} width="100%" onClick={() => history.push('/')} />
         <Gap width={50} />
-        <Button label="Save" background={BLUE} height={35} width="100%" />
+        <Button label="Save" background={BLUE} height={35} width="100%" onClick={() => setsweetWarning(true)} />
       </div>
-    </div>
+    </div >
   )
 }
 
